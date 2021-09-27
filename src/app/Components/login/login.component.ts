b649ed4b4df26a4fc84d594fe6e6fac33d781a1e
MOHAMEDAfrath <afrathismath@gmail.com>
2021-09-27 18:02:24 +0530

    [Afrath]:-Added form validation

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  RegisterForm!:FormGroup
   hide = true;
   signup = false;
   ngOnInit(): void {
+    this.RegisterForm = new FormGroup({
+      FullName : new FormControl('',[Validators.required,Validators.minLength(3)]),
+      Email:new FormControl('',[Validators.required,Validators.email]),
+      Password: new FormControl('',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}')]),
+      Mobile : new FormControl('',[Validators.required,Validators.minLength(10),Validators.pattern('^[0-9]{10}$')])
+    })
   }
 
 }
