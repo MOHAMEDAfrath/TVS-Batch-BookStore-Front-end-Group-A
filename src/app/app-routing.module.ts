4ecf4aea5cc6f31b3baedcfe06a24dd6f06d7c38
asmithasatesh <asmithasatesh@gmail.com>
2021-09-28 17:53:40 +0530

    [Asmitha] Refactor: Add Reset password path in App routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'forgotPassword', component: ForgotPasswordComponent },
   {path:'login',component:LoginComponent},
-  {path:'resetPassword',component:ResetPasswordComponent}
+  {path:'resetPassword',component:ResetPasswordComponent},
+  {path:'home',component:HomeComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)
