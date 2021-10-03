18e20b7d386a77ff7066b0264304feb49a4956db
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-10-03 14:27:03 +0530

    [SaiVijayaKumar]Refactor:scss for fotter and add to cart service

--- src/app/Components/book-description/book-description.component.ts
+++ src/app/Components/book-description/book-description.component.ts
@@ -9,46 +9,51 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./book-description.component.scss']
 })
 export class BookDescriptionComponent implements OnInit {
-  @Input() bookdetails!:any
-  constructor(private book:BookService, private cartService : CartService,private wishlist:WishlistService,private snackBar:MatSnackBar) { }
-  Userrating=[{name:'Aniket Chile',
-               rating:3,
-               review:'Good product. Even though the translation could have been better, Chanaky\'s neeti are thought provoking. Chanakya has written on many different topics and his writings are succinct.'}
-               ,
-            {
-              name:'Shweta Bodkar',
-               rating:4,
-              review:'Good product. Even though the translation could have been better, Chanaky\'s neeti are thought provoking. Chanakya has written on many different topics and his writings are succinct.'}
-            ]
+  @Input() bookdetails!: any
+  constructor(private book: BookService, private cartService: CartService, private wishlist: WishlistService, private snackBar: MatSnackBar) { }
+  Userrating = [{
+    name: 'Aniket Chile',
+    rating: 3,
+    review: 'Good product. Even though the translation could have been better, Chanaky\'s neeti are thought provoking. Chanakya has written on many different topics and his writings are succinct.'
+  }
+    ,
+  {
+    name: 'Shweta Bodkar',
+    rating: 4,
+    review: 'Good product. Even though the translation could have been better, Chanaky\'s neeti are thought provoking. Chanakya has written on many different topics and his writings are succinct.'
+  }
+  ]
 
   ngOnInit(): void {
   }
-  Resize(){
-    var textArea = document.getElementById("textarea")!      
+  Resize() {
+    var textArea = document.getElementById("textarea")!
     textArea.style.height = 'auto';
-    textArea.style.width='auto';
-    textArea.style.height = Math.min(500,textArea.scrollHeight) + 'px';
-    textArea.style.width=textArea.scrollWidth+'px';
+    textArea.style.width = 'auto';
+    textArea.style.height = Math.min(500, textArea.scrollHeight) + 'px';
+    textArea.style.width = textArea.scrollWidth + 'px';
   }
-  AddtoWishList()
-  {
+  AddtoWishList() {
     this.wishlist.AddtoWishList(this.bookdetails)
-    .subscribe((result: any) => {
-      this.snackBar.open(result.message, '', { duration: 3000, verticalPosition: 'bottom', horizontalPosition: 'left' });
-      console.log(result);
-    }, error => {
-      this.snackBar.open(`${error.error.message}`, '', { duration: 3000, verticalPosition: 'bottom', horizontalPosition: 'left' });
-    })
+      .subscribe((result: any) => {
+        this.snackBar.open(result.message, '', { duration: 3000, verticalPosition: 'bottom', horizontalPosition: 'left' });
+        console.log(result);
+      }, error => {
+        this.snackBar.open(`${error.error.message}`, '', { duration: 3000, verticalPosition: 'bottom', horizontalPosition: 'left' });
+      })
   }
 
-    
-AddBooktoCart()
-{
-  console.log("working");
-  this.cartService.AddBooktoCart(this.bookdetails)
-  .subscribe((result:any)=>{
-    console.log(result.message);
-  })
-}
 
+  AddBooktoCart() {
+    if (this.bookdetails['bookQuantity'] > 0) {
+      console.log("working");
+      this.cartService.AddBooktoCart(this.bookdetails)
+        .subscribe((result: any) => {
+          console.log(result.message);
+        })
+    }
+    else {
+      this.snackBar.open("Out of Stock! Cant add to  cart", '', { duration: 3000, verticalPosition: 'bottom', horizontalPosition: 'left' });
+    }
+  }
 }
